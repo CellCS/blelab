4ed6c5c13ace2492d7a0a9cc0e535be4d40d4bfa
CellCS <cellcs@m1>
2024-04-04 00:03:46 -0400

    update version

--- blelab/lib/utils/app_constants.dart
+++ blelab/lib/utils/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appId = 'BLElab';
-  static const String appVersion = '1.0.0';
+  static const String appVersion = '1.0.1';
   static const String appDefaultlocal = 'en';
 
   // routes
